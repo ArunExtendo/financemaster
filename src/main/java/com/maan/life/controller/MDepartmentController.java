b77226c59ae7099e99235fff6358d6fed355b38b
indrabalanR <indrabalan@extendotech.com>
2022-06-08 10:44:54 +0530

    M-Appparameter and M-Company and M-Department API's

---
+++ src/main/java/com/maan/life/controller/MDepartmentController.java
@@ -0,0 +1,154 @@
+package com.maan.life.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maan.life.bean.MDepartment;
+import com.maan.life.dto.ListViewParam;
+import com.maan.life.response.Response;
+import com.maan.life.response.ResponseGenerator;
+import com.maan.life.response.TransactionContext;
+import com.maan.life.service.MDepartmentService;
+import com.maan.life.service.MessagePropertyService;
+import com.maan.life.util.Convention;
+import com.maan.life.util.ValidationUtil;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@AllArgsConstructor(onConstructor_ = { @Autowired })
+@RequestMapping("/mdepartment")
+public class MDepartmentController {
+
+	@Autowired
+	private MDepartmentService entityService;
+	private MessagePropertyService messageSource;
+	private @NonNull ResponseGenerator responseGenerator;
+	private Convention sorting;
+
+	private static final Logger logger = Logger.getLogger(MDepartmentController.class);
+
+	@PostMapping(value = "/createOrUpdate", produces = "application/json")
+	public ResponseEntity<?> createOrUpdate(
+			@ApiParam(value = "Request payload") @Valid @RequestBody MDepartment request,
+			@RequestHeader HttpHeaders httpHeader) throws Exception {
+
+		TransactionContext context = responseGenerator.generateTransationContext(httpHeader);
+
+		Optional<MDepartment> obj = entityService.findByDeptCode(request.getDeptCode());
+		if (obj.isPresent()) {
+			String[] param = new String[] { request.getDeptCode() };
+			return responseGenerator.errorResponse(context, messageSource.getMessage("mdept.code.duplicate", param),
+					HttpStatus.OK);
+		}
+
+		entityService.saveorupdate(request);
+
+		try {
+			return responseGenerator.successResponse(context, messageSource.getMessage("saved"), HttpStatus.OK);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			logger.error(e.getMessage(), e);
+			return responseGenerator.errorResponse(context, e.getMessage(), HttpStatus.BAD_REQUEST);
+
+		}
+
+	}
+
+	@ApiOperation(value = "Allows to fetch all data List.", response = Response.class)
+	@GetMapping(value = "/getAll", produces = "application/json")
+	public ResponseEntity<?> getAll(@RequestHeader HttpHeaders httpHeader) throws Exception {
+		TransactionContext context = responseGenerator.generateTransationContext(httpHeader);
+
+		try {
+
+			List<MDepartment> lst = entityService.getAll();
+			return responseGenerator.successGetResponse(context, messageSource.getMessage("fetched"), lst,
+					HttpStatus.OK);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			logger.error(e.getMessage(), e);
+			return responseGenerator.errorResponse(context, e.getMessage(), HttpStatus.BAD_REQUEST);
+
+		}
+	}
+
+	@ApiOperation(value = "Allows to fetch Grid List.", response = Response.class)
+	@PostMapping(value = "/getAll", produces = "application/json")
+	public ResponseEntity<?> getAll(@RequestBody ListViewParam request, @RequestHeader HttpHeaders httpHeader)
+			throws Exception {
+
+		TransactionContext context = responseGenerator.generateTransationContext(httpHeader);
+
+		Pageable paging = sorting.getPaging(sorting.getPageNumber(request.getPageNumber()),
+				sorting.getPageSize(request.getPageSize()));
+
+		try {
+
+			List<MDepartment> obj = new ArrayList<MDepartment>();
+			Page<MDepartment> list = null;
+			if (ValidationUtil.isNull(request.getSearch())) {
+
+				list = entityService.findAll(paging);
+				obj = list.getContent();
+			} else {
+				list = entityService.findSearch(request.getSearch(), paging);
+				obj = list.getContent();
+
+			}
+			if (request.getCode() != null) {
+				List<String> o = new ArrayList<String>();
+				for (String ob : request.getCode()) {
+					o.add(ob);
+				}
+				list = entityService.findByDeptCompCodeAndDeptDivnCode(o.get(0), o.get(1), paging);
+				obj = list.getContent();
+			}
+			Map<String, Object> response = new HashMap<>();
+			response.put("data", obj);
+			response.put("currentPage", list.getNumber());
+			response.put("totalItems", list.getTotalElements());
+			response.put("totalPages", list.getTotalPages());
+
+			return responseGenerator.successGetResponse(context, messageSource.getMessage("fetched"), response,
+					HttpStatus.OK);
+
+		} catch (
+
+		Exception e) {
+			e.printStackTrace();
+			logger.error(e.getMessage(), e);
+			return responseGenerator.errorResponse(context, e.getMessage(), HttpStatus.BAD_REQUEST);
+
+		}
+	}
+
+}
